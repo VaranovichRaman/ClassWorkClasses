62a627be426971d01084520c574a1da5c77f2671
Leonid Radkevich <laradkevich@gmail.com>
2019-10-09 20:25:01 +0300

    kris is now Tolchok

---
+++ ClassWorkClasses/Models/Tolchok.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWorkClasses.Models
+{
+    public class Tolchok
+    {
+        public string Name { get; set; }
+
+        public int Age { get { return Years(DateOfBirth, DateTime.Now); } }
+        public DateTime DateOfBirth { get; set; }
+
+        private int Years(DateTime start, DateTime end)
+        {
+            int difference = (end.Year - start.Year - 1) +
+                (((end.Month > start.Month) ||
+                ((end.Month == start.Month) && (end.Day >= start.Day)))
+                ? 1 : 0);
+            return difference;
+        }
+    }
+}
--- ClassWorkClasses/Program.cs
+++ ClassWorkClasses/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ClassWorkClasses.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,10 @@ namespace ClassWorkClasses
     {
         static void Main(string[] args)
         {
+            var kris = new Tolchok();
+            kris.DateOfBirth = new DateTime(1994, 6, 7);
+            Console.WriteLine($"age is {kris.Age}");
+            Console.ReadLine();
         }
     }
 }
