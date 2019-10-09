af6a39148cb2343f6feb0ba261e73f983f6436d3
Leonid Radkevich <laradkevich@gmail.com>
2019-10-09 20:42:23 +0300

    added datetime extension

---
+++ ClassWorkClasses/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWorkClasses.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int GetYearsDiff(this DateTime date, DateTime another)
+        {
+            int difference = (another.Year - date.Year - 1) +
+                (((another.Month > date.Month) ||
+                ((another.Month == date.Month) && (another.Day >= date.Day)))
+                ? 1 : 0);
+            return difference;
+        }
+    }
+}
--- ClassWorkClasses/Models/Tolchok.cs
+++ ClassWorkClasses/Models/Tolchok.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassWorkClasses.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace ClassWorkClasses.Models
     {
         public string Name { get; set; }
 
-        public int Age { get { return Years(DateOfBirth, DateTime.Now); } }
+        public int Age { get { return DateOfBirth.GetYearsDiff(DateTime.Now); } }
         public DateTime DateOfBirth { get; set; }
 
         private int Years(DateTime start, DateTime end)
