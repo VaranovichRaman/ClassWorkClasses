64da90979a03731e61df127c96c84c082de18649
Raven Raven <raven_180691@mail.ru>
2019-10-16 23:53:39 +0300

    Another commit

--- ClassWorkClasses/Program.cs
+++ ClassWorkClasses/Program.cs
@@ -24,8 +24,8 @@ namespace ClassWorkClasses
             
             
             TolchokCreation.Start(flag, tolchOldCampClub, tolchNewCampClub, tolchSwampCampClub);
-            
-            
+
+            Console.WriteLine($"Old Camp members:");
             foreach (var item in tolchOldCampClub.MembersOfClub)
             {
                 Console.WriteLine($"\n {item.Name} in {item.ClubInfo.ClubName}, head of club: {item.ClubInfo.HeadOfClubName}, favorite material: {item.FavoriteWeapon.MaterialOfWeapon}");
@@ -35,11 +35,31 @@ namespace ClassWorkClasses
                     Console.WriteLine($"\n{item2.GameName}");
                 }
             }
+            Console.WriteLine($"New Camp members:");
+            foreach (var item in tolchNewCampClub.MembersOfClub)
+            {
+                Console.WriteLine($"\n {item.Name} in {item.ClubInfo.ClubName}, head of club: {item.ClubInfo.HeadOfClubName}, favorite material: {item.FavoriteWeapon.MaterialOfWeapon}");
+                Console.WriteLine($"His/her games:");
+                foreach (var item2 in item.TolchoksGames)
+                {
+                    Console.WriteLine($"\n{item2.GameName}");
+                }
+            }
+            Console.WriteLine($"Swamp Camp members:");
+            foreach (var item in tolchSwampCampClub.MembersOfClub)
+            {
+                Console.WriteLine($"\n {item.Name} in {item.ClubInfo.ClubName}, head of club: {item.ClubInfo.HeadOfClubName}, favorite material: {item.FavoriteWeapon.MaterialOfWeapon}");
+                Console.WriteLine($"His/her games:");
+                foreach (var item2 in item.TolchoksGames)
+                {
+                    Console.WriteLine($"\n{item2.GameName}");
+                }
+            }
 
-            
 
 
-            
+
+
             //var newDate = new DateTime(1945, 5, 9);
             //Console.WriteLine($"{newDate.GetYearsDiff(DateTime.Now)} years from victory of {newDate.GetCentury()} century greatest war"); 
             Console.ReadLine();
